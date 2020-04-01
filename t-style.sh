8ba6ce2c840de54028cf013302eb6c6cbd40c716
Inderveerjoney <62381493+Inderveerjoney@users.noreply.github.com>
2020-04-01 09:37:40 +0530

    Add files via upload

---
+++ t-style.sh
@@ -0,0 +1,39 @@
+echo "        
+           ################################
+           #  666666666   6666666       ###
+           #      6       6             ###
+           #      6       6666666       ###
+           #      6             6       ###
+           #      6.      6666666       ###
+           #                            ### 
+           ################################ 
+"
+echo ""
+echo ""
+echo ""
+ls
+bash pkg.txt
+clear
+cat  joney.txt
+sleep 2.0
+clear
+echo ""
+echo""
+cd /data/data/com.termux/files/usr/etc
+ls
+rm -rf bash.bashrc
+cd
+ls
+cd Terstyl
+ls
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+clear
+ls
+cp music.mp3 /sdcard
+ls
+cd
+clear
+clear
+clear
+clear
+
